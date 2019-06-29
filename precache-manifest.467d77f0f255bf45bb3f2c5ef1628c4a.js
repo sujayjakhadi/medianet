cab12013e98eb0a1da87b2eb7d6dae554e32f7b5
sujay.jakhadi@gmail.com <sujay.jakhadi@gmail.com>
2019-06-29 21:26:31 +0530

    Updates

---
+++ precache-manifest.467d77f0f255bf45bb3f2c5ef1628c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda35c536b60898a0648a057fe578037",
+    "url": "/medianet/index.html"
+  },
+  {
+    "revision": "1abc6f70d366c5576444",
+    "url": "/medianet/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "036f04228bec2b2f5ead",
+    "url": "/medianet/static/css/main.11ca88e0.chunk.css"
+  },
+  {
+    "revision": "1abc6f70d366c5576444",
+    "url": "/medianet/static/js/2.6db17712.chunk.js"
+  },
+  {
+    "revision": "036f04228bec2b2f5ead",
+    "url": "/medianet/static/js/main.60524b1a.chunk.js"
+  },
+  {
+    "revision": "feba5bbfbe5ced16922a",
+    "url": "/medianet/static/js/runtime~main.b146db80.js"
+  }
+]);
